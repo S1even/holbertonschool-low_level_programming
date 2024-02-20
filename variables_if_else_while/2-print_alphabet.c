491eb181aadda6b76aa1438ae37da7eae24774ee
S1even <stevendevin95520@hotmail.com>
2024-02-20 01:17:19 -0800

    I play what I call the alphabet game

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+  * print_alphabet - Make alphabet x10 times
+  *
+  * Return: void
+  */
+void print_alphabet(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
+}
