d13bfb537ac0d9ac3d59051a52b5c954a1c3255f
S1even <shadeauw1@gmail.com>
2024-06-18 13:59:49 +0200

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = '0';
+
+	for (ch = '0'; ch <= '9'; ch++)
+
+	putchar(ch);
+
+	{
+		char ch = 'a';
+
+		for (ch = 'a'; ch <= 'f'; ch++)
+
+		putchar(ch);
+	}
+
+		putchar('\n');
+
+		return (0);
+}
