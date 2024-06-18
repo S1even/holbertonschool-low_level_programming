2c97c2e88748b62bf3a6ffdda960c70de7a8dd08
S1even <shadeauw1@gmail.com>
2024-06-18 11:22:45 +0200

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	putchar(ch);
+
+	{
+		char ch = 'A';
+
+		for (ch = 'A'; ch <= 'Z'; ch++)
+
+		putchar(ch);
+										}
+
+		putchar('\n');
+
+		return (0);
+}
