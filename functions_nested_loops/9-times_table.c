705e1e6b24a56e297e4db731631b79a6e08fce04
S1even <shadeauw1@gmail.com>
2024-06-20 13:53:39 +0200

    Learn your times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,27 +6,34 @@
 
 void times_table(void)
 {
-	int num, mult, result;
+	int i, h;
+	int value;
 
-	for (num = 0; num <= 9; num++)
+	for (h = 0; h <= 9; h++)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
+		for (i = 0; i <= 9; i++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			value = h * i;
 
-			result = num * mult;
-
-			if (result <= 9)
+			if (i != 0)
+			{
+				_putchar(',');
 				_putchar(' ');
+				if (value < 10)
+				{
+					_putchar (' ');
+				}
+			}
+			if (value >= 10)
+			{
+				_putchar((value / 10) + '0');
+				_putchar((value % 10) + 0);
+			}
 			else
-			{	_putchar((result / 10) + '0');
-
-				_putchar((result % 10) + '0');
+			{
+				_putchar(value + '0');
 			}
 		}
-				_putchar('\n');
+		_putchar('\n');
 	}
 }
