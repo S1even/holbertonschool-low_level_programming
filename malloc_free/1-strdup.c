bf46801b3cd5bbc30fbda6d4a09c4af5aa164c93
S1even <shadeauw1@gmail.com>
2024-07-08 12:03:09 +0200

    The woman who has no imagination has no wings

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -27,7 +27,7 @@ char *_strdup(char *str)
 	if (duplicate == NULL)
 		return (NULL);
 
-	for (i = 0; i < length; i++)
+	for (i = 0; str[i] ; i++)
 		duplicate[i] = str[i];
 
 	duplicate[length] = '\0';
