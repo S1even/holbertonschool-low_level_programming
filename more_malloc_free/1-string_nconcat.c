9c2e3276db165fb44764413936e31e3b5c29aff7
S1even <shadeauw1@gmail.com>
2024-07-09 12:35:05 +0200

    string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,47 +1,47 @@
 #include "main.h"
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * string_nconcat - function that concatenates two strings
+ * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
  * @n: number of bytes
- * Return: result
+ * Return: result or NULL
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len = n, i;
 	char *result;
+	unsigned int i, j, k, l;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i]; i++)
-		len++;
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-	for (i = 0; s2[i]; i++)
-		len++;
-
-	result = malloc(sizeof(char) * (n + len + 1));
-
+	if (j < n)
+		j = n;
+	k = i + n;
+	result = malloc(sizeof(char) * (k + 1));
 	if (result == NULL)
 	{
 		free(result);
 		return (NULL);
 	}
-	len = 0;
 
-	for (i = 0; s1[i]; i++)
-		result[len++] = s1[i];
+	for (l = 0; l < k; l++)
+		if (l < i)
+			result[l] = s1[l];
+		else
+			result[l] = s2[l - i];
 
-	for (i = 0; s2[i] && i < n; i++)
-		result[len++] = s2[i];
-
-	result[len + i] = '\0';
+	result[k] = '\0';
 
 	return (result);
 }
