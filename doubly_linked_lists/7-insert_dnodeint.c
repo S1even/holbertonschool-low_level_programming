9202f0960ea714589f1282c16ad81f96a0c9fca9
S1even <Stevendevin95520@hotmail.com>
2024-07-30 16:21:37 +0200

    fix issue

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,36 +10,40 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_nodes;
-	dlistint_t *current_node = *h;
-
+	dlistint_t *new_node, *current_node = *h;
 	unsigned int i = 0;
 
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
 	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	while (current_node)
 	{
-		if (i == idx - 1)
+		return (add_dnodeint(h, n));
+	}
+
+	for (i = 0; i < idx - 1; i++)
+	{
+		if (current_node == NULL)
 		{
-			if (current_node->next == NULL)
-				return (add_dnodeint_end(h, n));
-
-			new_nodes = malloc(sizeof(dlistint_t));
-
-			if (new_nodes == NULL)
-				return (NULL);
-
-			new_nodes->n = n;
-			new_nodes->prev = current_node;
-			current_node->next->prev = new_nodes;
-			current_node->next = new_nodes;
-			return (new_nodes);
+			free(new_node);
+			return (NULL);
 		}
-		i++;
-
 		current_node = current_node->next;
 	}
 
-	return (NULL);
+	if (current_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = current_node->next;
+	new_node->prev = current_node;
+	if (current_node->next != NULL)
+		current_node->next->prev = new_node;
+	current_node->next = new_node;
+
+	return (new_node);
 }
