0f72476729aeb91f6735dd7550dc8849a4dda19b
S1even <Stevendevin95520@hotmail.com>
2024-07-30 10:07:24 +0200

    Print list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -10,11 +10,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count_nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		count_nodes++;
+		h = h->next;
 	}
 
 	return (count_nodes);
