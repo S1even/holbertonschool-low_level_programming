7fab4425cf6b541a2e684b6c936b43974a955065
S1even <Stevendevin95520@hotmail.com>
2024-08-01 10:57:01 +0200

    Update header

--- file_io/main.h
+++ file_io/main.h
@@ -12,6 +12,8 @@ int _strlen(char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int main(int ac, char *av[]);
+int main(int argc, char *argv[]);
+void _error(int status, char *v);
+void close_file(int fd);
 
 #endif
